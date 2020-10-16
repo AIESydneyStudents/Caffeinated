12a2749cf53aa2c9a6a517fb1bdce10187a1a070
Tim <timhinshelwood@gmail.com>
2020-10-16 12:47:04 +1100

    Feature(PauseMenu): #46: Added basic pause menu
    Main Menu needed for full functionality

---
+++ project/Assets/Scripts/UI_PauseScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_PauseScript : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    //public AudioSource sound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    //public void LoadMenu()
+    //{
+        //isPaused = false;
+        //Time.timeScale = 1f;
+       // SceneManager.LoadScene("MainMenu");
+    //}
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Pause()
+    {
+        //sound.Play();
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+}
