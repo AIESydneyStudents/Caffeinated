1088e5ebe66399cc988dd9e253ba13a9b4dcd34c
Logan Ryan <loganryan982@gmail.com>
2020-11-19 10:06:22 +1100

    Fix (Particles) : #119 : Fixed particles

--- project/Assets/Scripts/PlayerParticleEffectController.cs
+++ project/Assets/Scripts/PlayerParticleEffectController.cs
@@ -13,15 +13,19 @@ public class PlayerParticleEffectController : MonoBehaviour
     GameObject wallParticleEffect;
     [SerializeField]
     Vector3 dustParticlesOffset;
+    [SerializeField]
+    Vector3 wallParticlesOffset;
+    Transform playerTranform;
 
     RB_PlayerController playerController;
     private int jumps = 0;
-    private bool playerInTheAir;
+    private float timeInAir;
     
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<RB_PlayerController>();
+        playerTranform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -33,7 +37,6 @@ public class PlayerParticleEffectController : MonoBehaviour
             {
                 Instantiate(jumpParticleEffect, gameObject.transform.position, Quaternion.Euler(90, 0, 0));
                 jumps++;
-                playerInTheAir = true;
             }
         }
 
@@ -47,16 +50,30 @@ public class PlayerParticleEffectController : MonoBehaviour
         }
 
         // Dust particles are played if the player was in the air and just landed
-        if (playerInTheAir && playerController.grounded)
+        if (!playerController.grounded)
+        {
+            timeInAir += Time.deltaTime;
+        }
+
+        if (timeInAir > 0 && playerController.grounded)
         {
             Instantiate(dustParticleEffect, gameObject.transform.position + dustParticlesOffset, Quaternion.Euler(90, 0, 0));
-            playerInTheAir = false;
+            timeInAir = 0;
         }
 
         // Wall grind dust plays when player is on the wall
         if (playerController.curHitObjectW != null && !playerController.grounded)
         {
-            Instantiate(wallParticleEffect, gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+            Instantiate(wallParticleEffect, gameObject.transform.position + wallParticlesOffset, Quaternion.Euler(90, 0, 0));
+        }
+
+        if (playerTranform.eulerAngles.y == 90 && wallParticlesOffset.x < 0)
+        {
+            wallParticlesOffset.x = -wallParticlesOffset.x;
+        }
+        else if (playerTranform.eulerAngles.y == 270 && wallParticlesOffset.x > 0)
+        {
+            wallParticlesOffset.x = -wallParticlesOffset.x;
         }
     }
 }
