16dfa2926777e90539e86458887372d4596e6c3b
madcrafty <madcrafty@icloud.com>
2020-11-19 11:48:58 +1100

    Bounce Pad Animations

--- project/Assets/Scripts/BouncePad.cs
+++ project/Assets/Scripts/BouncePad.cs
@@ -6,17 +6,24 @@ public class BouncePad : MonoBehaviour
 {
     public float BounceForce;
     public string[] JumpTagBlacklist;
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.GetComponent<Rigidbody>() != null)
-    //    {
-    //        other.GetComponent<Rigidbody>().velocity = transform.up * BounceForce;
-    //    }        
-    //}
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null && !BlackListCheck(other.tag))
+        {
+            anim.SetTrigger("playerColided");
+            //other.GetComponent<Rigidbody>().velocity = transform.up * BounceForce;
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
         if (other.GetComponent<Rigidbody>() != null && !BlackListCheck(other.tag))
         {
+            //anim.SetTrigger("playerColided");
             other.GetComponent<Rigidbody>().velocity = transform.up * BounceForce;
         }
     }
